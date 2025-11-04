0db7393dde5812154480a099dcbafd639c7f75af
AungKaung <aungkaung855@gmail.com>
2025-11-04 23:59:46 +0000

    Update car.ino

--- car.ino
+++ car.ino
@@ -1,1 +1,96 @@
+#include <SoftwareSerial.h>
+
+// Motor pins
+const int IN1 = 4;
+const int IN2 = 5;
+const int IN3 = 6;
+const int IN4 = 7;
+const int ENA = 9;
+const int ENB = 10;
+
+// Bluetooth pins (connect to DFR0781 TX/RX)
+SoftwareSerial BTSerial(8, 9); // RX, TX
+
+void setup() {
+  Serial.begin(9600);
+  BTSerial.begin(9600);
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
+  stopCar();
+  Serial.println("Car ready — waiting for Bluetooth commands...");
+}
+
+void loop() {
+  if (BTSerial.available()) {
+    char cmd = BTSerial.read();
+    Serial.print("Received: ");
+    Serial.println(cmd);
+
+    switch (cmd) {
+      case '2': goForward(); break;
+      case '8': goBackward(); break;
+      case '4': turnLeft(); break;
+      case '6': turnRight(); break;
+      case '5': stopCar(); break;
+      default:  stopCar(); break;
+    }
+  }
+}
+
+// Movement functions
+void goForward() {
+  Serial.println("Forward");
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, 200);
+  analogWrite(ENB, 200);
+}
+
+void goBackward() {
+  Serial.println("Backward");
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENA, 200);
+  analogWrite(ENB, 200);
+}
+
+void turnLeft() {
+  Serial.println("Left");
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, 180);
+  analogWrite(ENB, 180);
+}
+
+void turnRight() {
+  Serial.println("Right");
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENA, 180);
+  analogWrite(ENB, 180);
+}
+
+void stopCar() {
+  Serial.println("Stop");
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+}
 
